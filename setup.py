49259b6699bfc99d5b52b8c66c7fbf142f3ebf4e
joaquin <j>
2020-05-24 13:28:00 -0700

    removed long description

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding="utf-8") as fd:
-        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
-
-
 def get_install_requires():
     with open("requirements.txt", "r") as f:
         return [line.strip() for line in f.readlines() if not line.startswith("-")]
@@ -25,7 +18,6 @@ setup(
     license="MIT",
     author="vincentlv",
     description="litho simulation",
-    long_description=read("README.md"),
     packages=find_packages(exclude=("tests",)),
     install_requires=get_install_requires(),
     python_requires=">=3.6",
